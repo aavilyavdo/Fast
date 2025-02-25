d5182d8b3002a1c8e204090035b8f87310967dcd
aavilyavdo <aavilyavdo@yandex.ru>
2025-02-25 14:02:04 +0300

    Create ArticleController.php

---
+++ ArticleController.php
@@ -0,0 +1,64 @@
+<?
+// app/Http/Controllers/ArticleController.php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::all();
+        return view('articles.index', compact('articles'));
+    }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+        $article = Article::create([
+            'title' => $validated['title'],
+            'content' => $validated['content'],
+            'user_id' => auth()->id(),
+        ]);
+
+        return redirect()->route('articles.index')->with('success', 'Статья успешно добавлена!');
+    }
+
+    public function edit($id)
+    {
+        $article = Article::findOrFail($id);
+        return view('articles.edit', compact('article'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+        $article = Article::findOrFail($id);
+        $article->update($validated);
+
+        return redirect()->route('articles.index')->with('success', 'Статья успешно обновлена!');
+    }
+
+    public function destroy($id)
+    {
+        $article = Article::findOrFail($id);
+        $article->delete();
+
+        return redirect()->route('articles.index')->with('success', 'Статья успешно удалена!');
+    }
+}?>
