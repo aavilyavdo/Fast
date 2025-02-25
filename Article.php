124abde475e8471a6aa56c7d70182d837a156a9a
aavilyavdo <aavilyavdo@yandex.ru>
2025-02-25 14:18:56 +0300

    Update Article.php

--- Article.php
+++ Article.php
@@ -1,4 +1,4 @@
-<?// app/Models/Article.php
+<?php
 
 namespace App\Models;
 
@@ -9,10 +9,30 @@ class Article extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'content', 'user_id'];
+    // Указываем таблицу, если название отличается от множественного числа класса
+    protected $table = 'Articles';
 
+    // Указываем, какие атрибуты можно массово присваивать
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id',
+        'created_at',
+        'updated_at',    ];
+
+    // Указываем, какие атрибуты должны быть скрыты в массиве и JSON представлении
+    protected $hidden = [
+       
+    ];
+
+    // Определяем отношения с другими моделями
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
     }
 }
